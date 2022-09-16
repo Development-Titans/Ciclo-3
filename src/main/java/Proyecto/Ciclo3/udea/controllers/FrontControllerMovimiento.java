354dd65063f9d3e2ef89421aa0205c6971a4e4e4
Cristian Alejandro Arroayave <cristianalejo84@gmail.com>
2022-09-16 17:29:01 +0200

    Correccion error

--- src/main/java/Proyecto/Ciclo3/udea/controllers/FrontControllerMovimiento.java
+++ src/main/java/Proyecto/Ciclo3/udea/controllers/FrontControllerMovimiento.java
@@ -7,8 +7,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 
 import java.util.List;
 
@@ -25,7 +23,7 @@ public class FrontControllerMovimiento {
     }
 
     //@RequestMapping(value="/",method = RequestMethod.GET)
-    @GetMapping("/")
+    @GetMapping("/index")
     public String index(){
         return "index";
     }
