341a8d88b5ff865b1a49afbca897fa3631241636
Cristian Alejandro Arroayave <cristianalejo84@gmail.com>
2022-09-21 18:24:32 +0200

    Creando vista del Login

--- src/main/java/Proyecto/Ciclo3/udea/frontControllers/FrontControllerIndex.java
+++ src/main/java/Proyecto/Ciclo3/udea/frontControllers/FrontControllerIndex.java
@@ -10,4 +10,9 @@ public class FrontControllerIndex {
     public String index(){
         return "index";
     }
+
+    @GetMapping("/login")
+    public String login(){
+        return "login";
+    }
 }
