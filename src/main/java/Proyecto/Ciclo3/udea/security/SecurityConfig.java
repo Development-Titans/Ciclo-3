dc62cd4bcbb6da2df2ff69d42d328cdb5963ce4d
Claudia Cely <ccelyc@gmail.com>
2022-09-25 23:21:18 -0500

    Ajustando cositas varias

--- src/main/java/Proyecto/Ciclo3/udea/security/SecurityConfig.java
+++ src/main/java/Proyecto/Ciclo3/udea/security/SecurityConfig.java
@@ -42,7 +42,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter{
     @Override
     protected void configure(HttpSecurity http) throws Exception{
 
-        String[] permitted = new String[]{"/resources/**", "/static/img/**", "/static/styles/**"};
+        String[] permitted = new String[]{"/resources/**", "/img/**", "/css/**"};
 
         http.authorizeRequests()
                 .antMatchers(permitted).permitAll()
