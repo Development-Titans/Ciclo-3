1da1677f60c036de36869c9942852f70e5f4dcb2
Claudia Cely <ccelyc@gmail.com>
2022-09-25 22:56:29 -0500

    cambiando Static por static

--- src/main/java/Proyecto/Ciclo3/udea/security/SecurityConfig.java
+++ src/main/java/Proyecto/Ciclo3/udea/security/SecurityConfig.java
@@ -5,7 +5,6 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.core.userdetails.UserDetailsService;
@@ -43,7 +42,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter{
     @Override
     protected void configure(HttpSecurity http) throws Exception{
 
-        String[] permitted = new String[]{"/resources/**","/Static/img/**", "/Static/Styles/**"};
+        String[] permitted = new String[]{"/resources/**", "/static/img/**", "/static/Styles/**"};
 
         http.authorizeRequests()
                 .antMatchers(permitted).permitAll()
